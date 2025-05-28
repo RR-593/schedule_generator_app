1a7e4c7a5b88b9482a36ae27807fdf38f1a988de
Raphael Munoz <raphael@munoz.to>
2025-05-28 19:47:12 +1000

    Add editableSpans to card

--- src/ScheduleGeneratorFormComponents/ExerciseCard.js
+++ src/ScheduleGeneratorFormComponents/ExerciseCard.js
@@ -1,46 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import EditableTextSpan from "./edibleTextSapn";
 import "../StyleSheets/ExerciseCard.css";
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ escerciseTitle = '', resSets = '', onSave }) => {
+  const [eTitle, setETitle] = useState('excercsie');
+  const [eRepSet, setERepSet] = useState('n×n or just n ...');
+
+  useEffect(() => {
+    if (escerciseTitle.trim() !== '') setETitle(escerciseTitle);
+    if (resSets.trim() !== '') setERepSet(resSets);
+  }, [escerciseTitle,resSets]);
+
   return (
     <div className="container">
-      {/* Pull-Ups Card */}
-      <div className="card">
-        <h2>Pull-Ups</h2>
-        <p>10 reps x 10 sets</p>
-        <p>(Twice a Week)</p>
-        <textarea placeholder="Notes..." />
-      </div>
+      {false ? (
+        <div className="card"> {/* Pull-Ups Card */}
+          <div className="details">
+            <h3>Pull-Ups</h3>
+            <p>10 reps x 10 sets</p>
+            <p>(Twice a Week)</p>
+          </div>
+          <textarea placeholder="Notes..." />
+        </div>
+      ) : (
+        < div className="card" > {/* Generic Card */}
+          <div className="details">
+            <EditableTextSpan initialText={eTitle} />
+            <p> </p>
+            <EditableTextSpan initialText={eRepSet} />
+            {/* <label>Repeat:</label>
+            <select>
+              <option>None</option>
+              <option>Once</option>
+              <option>Twice</option>
+            </select> */}
+          </div>
+          <textarea placeholder="Notes..." />
+        </div>
+      )}
 
       {/* Campus Board Card */}
-      <div className="card">
-        <h2>Campus board</h2>
-        <input type="text" placeholder="Type 3×10 or 3 sets..." />
-        <select>
-          <option>Once a Week</option>
-          <option>Twice a Week</option>
-          <option>Three Times a Week</option>
-        </select>
+      {/* <div className="card">
+        <div className="details">
+          <h3>Campus board</h3>
+          <input type="text" placeholder="Type 3×10 or 3 sets..." />
+          <select>
+            <option>Once a Week</option>
+            <option>Twice a Week</option>
+            <option>Three Times a Week</option>
+          </select>
+        </div>
         <textarea placeholder="Notes..." />
-      </div>
+      </div> */}
 
-      {/* Generic Card */}
-      <div className="card">
-        <div className="input-row">
-          <input type="text" placeholder="Input" />
-        </div>
-        <div className="input-row">
-          <input type="text" placeholder="Set" />
-          <input type="text" placeholder="Reps" />
-        </div>
-        <label>Repeat:</label>
-        <select>
-          <option>None</option>
-          <option>Once</option>
-          <option>Twice</option>
-        </select>
-        <textarea placeholder="Notes..." />
-      </div>
     </div>
   );
 };
