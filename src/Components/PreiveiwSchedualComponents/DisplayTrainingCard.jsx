b2173a66a9f27fc54e3d7863aeffe2aaedf87d0d
Raphael Munoz <raphael@munoz.to>
2025-06-06 19:45:19 +1000

    Create a Dnd context for each card

---
+++ src/Components/PreiveiwSchedualComponents/DisplayTrainingCard.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+
+import ExerciseTimeCard from "./ExerciseTimeCard";
+
+import {
+  DndContext,
+  closestCorners,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragOverlay,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+  useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+
+// 📦 Make each block sortable
+const SortableExercise = ({ item, onResize }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: item.id });
+
+  if (transform) transform.scaleY = 1;
+
+  if (item.title)
+    return (
+      <ExerciseTimeCard
+        item={item}
+        listeners={listeners}
+        attributes={attributes}
+        setNodeRef={setNodeRef}
+        transform={transform}
+        transition={transition}
+        isDragging={isDragging}
+        onResize={(newHeight) => onResize(item.id, newHeight)}
+      />
+    );
+  else
+    return (
+      <div
+        ref={(el) => {
+          setNodeRef(el);
+        }}
+        {...listeners}
+        {...attributes}
+        style={{
+          height: `${item.height / 5}px`,
+          opacity: 0.2,
+          zIndex:-1,
+          pointerEvents: "none",
+          backgroundColor: "white",
+          transform: CSS.Transform.toString(transform),
+          transition,
+        }}
+      ></div>
+    );
+};
+
+const DisplayTrainingCard = ({ data, cardHeight }) => {
+  const [sessionData, setSessionData] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+  const activeItem = sessionData.find((item) => item.id === activeId);
+
+  const handleDragStart = (event) => {
+    setActiveId(event.active.id);
+  };
+
+  const handleDragEnd = (event) => {
+    setActiveId(null);
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      const oldIndex = sessionData.findIndex((i) => i.id === active.id);
+      const newIndex = sessionData.findIndex((i) => i.id === over?.id);
+      setSessionData((items) => arrayMove(items, oldIndex, newIndex));
+    }
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: { distance: 5 },
+    })
+  );
+
+  return (
+    <div className="timeCards">
+      <DndContext
+        sensors={sensors}
+        onDragStart={handleDragStart}
+        collisionDetection={closestCorners}
+        onDragEnd={handleDragEnd}
+        modifiers={[restrictToVerticalAxis]}
+      >
+        <SortableContext
+          items={sessionData.map((item) => item.id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {sessionData.map((item) => (
+            <SortableExercise
+              key={item.id}
+              item={{
+                ...item,
+                height: item.height ?? cardHeight(),
+              }}
+              onResize={() => {}}
+            />
+          ))}
+        </SortableContext>
+        <DragOverlay>
+          {activeItem ? (
+            <ExerciseTimeCard item={activeItem} isOverlay={true} />
+          ) : null}
+        </DragOverlay>
+      </DndContext>
+    </div>
+  );
+};
+
+export default DisplayTrainingCard;
--- src/Components/PreiveiwSchedualComponents/PreviewTrainingSchedual.jsx
+++ src/Components/PreiveiwSchedualComponents/PreviewTrainingSchedual.jsx
@@ -1,22 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
-import ExerciseTimeCard from "./ExerciseTimeCard";
+import DisplayTrainingCard from "./DisplayTrainingCard";
 
-import {
-  DndContext,
-  closestCorners,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  DragOverlay,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  verticalListSortingStrategy,
-  useSortable,
-} from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
-import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 
 import CalanderHeader from "../ScheduleGeneratorFormComponents/CalanderHeader";
 import "../StyleSheets/PreviewTrainingSchedual.css";
@@ -77,84 +61,29 @@ const TimeSlot = ({ label }) => (
   </div>
 );
 
-// 📦 Make each block sortable
-const SortableExercise = ({ item, onResize }) => {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id: item.id });
-
-  if (transform) transform.scaleY = 1;
-
-  if (item.title)
-    return (
-      <ExerciseTimeCard
-        item={item}
-        listeners={listeners}
-        attributes={attributes}
-        setNodeRef={setNodeRef}
-        transform={transform}
-        transition={transition}
-        isDragging={isDragging}
-        onResize={(newHeight) => onResize(item.id, newHeight)}
-      />
-    );
-  else
-    return (
-      <div
-        ref={(el) => {
-          setNodeRef(el);
-        }}
-        {...listeners}
-        {...attributes}
-        style={{
-          height: `${item.height / 5}px`,
-          opacity: 0.2,
-          backgroundColor: "white",
-          transform: CSS.Transform.toString(transform),
-          transition,
-        }}
-      ></div>
-    );
-};
-
 const SessionView = () => {
   const displayCardAmount = 8;
   const [bodyHeight, setBodyHeight] = useState(window.innerHeight);
+  const cardHeight = () => bodyHeight / displayCardAmount;
 
-  const totalSpanCovered = initialSessionData.reduce((a, i) => a + i.span, 0);
-  const sData = Array.from(
-    { length: (displayCardAmount - 1) * 5 - totalSpanCovered },
-    (v, k) => k + 1
-  ).map((numberId) => {
-    let found = initialSessionData.find((item) => item.id === numberId + "");
-    return found
-      ? {
-          ...found,
-          height: found.span
-            ? found.span * (bodyHeight / displayCardAmount / 5)
-            : bodyHeight / displayCardAmount,
-        }
-      : { id: numberId };
+  // const totalSpanCovered = initialSessionData.reduce((a, i) => a + i.span, 0);
+
+  const sData = initialSessionData.map((item) => {
+    const resultArr = Array.from(
+      { length: (displayCardAmount - 1) * 5 - item.span },
+      (v, k) => k + 1
+    ).map((numberId) => {
+      return item.id === numberId + ""
+        ? {
+            ...item,
+            height: item.span ? item.span * (cardHeight() / 5) : cardHeight(),
+          }
+        : { id: numberId };
+    });
+    return resultArr;
   });
 
-  const [sessionData, setSessionData] = useState(sData);
-  const [activeId, setActiveId] = useState(null);
-  const activeItem = sessionData.find((item) => item.id === activeId);
-
   const myRef = useRef(null);
-
-  const handleResize = (id, newHeight) => {
-    setSessionData((prev) =>
-      prev.map((item) =>
-        item.id === id ? { ...item, height: newHeight } : item
-      )
-    );
-  };
 
   useEffect(() => {
     // console.log(myRef);
@@ -169,28 +98,6 @@ const SessionView = () => {
       setBodyHeight(newHeight);
     }
   }, []);
-
-  const sensors = useSensors(
-    useSensor(PointerSensor, {
-      activationConstraint: { distance: 5 },
-    })
-  );
-
-  const handleDragStart = (event) => {
-    setActiveId(event.active.id);
-    console.log(event.active);
-  };
-
-  const handleDragEnd = (event) => {
-    setActiveId(null);
-    const { active, over } = event;
-
-    if (active.id !== over?.id) {
-      const oldIndex = sessionData.findIndex((i) => i.id === active.id);
-      const newIndex = sessionData.findIndex((i) => i.id === over?.id);
-      setSessionData((items) => arrayMove(items, oldIndex, newIndex));
-    }
-  };
 
   return (
     <>
@@ -207,48 +114,17 @@ const SessionView = () => {
 
         <div className="exerciseTimeline">
           {/* Time Labels */}
-          <div
-            className="timmings"
-            style={{ gap: `${bodyHeight / displayCardAmount - 40}px` }}
-          >
+          <div className="timmings" style={{ gap: `${cardHeight() - 40}px` }}>
             {createTimeSlots("05:50", displayCardAmount, 30).map(
               (time, idx) => (
                 <TimeSlot key={idx} label={time} />
               )
             )}
           </div>
-
-          {/* Exercise Timeline */}
-          <div className="timeCards">
-            <DndContext
-              sensors={sensors}
-              onDragStart={handleDragStart}
-              collisionDetection={closestCorners}
-              onDragEnd={handleDragEnd}
-              modifiers={[restrictToVerticalAxis]}
-            >
-              <SortableContext
-                items={sessionData.map((item) => item.id)}
-                strategy={verticalListSortingStrategy}
-              >
-                {sessionData.map((item) => (
-                  <SortableExercise
-                    key={item.id}
-                    item={{
-                      ...item,
-                      height: item.height ?? bodyHeight / displayCardAmount,
-                    }}
-                    onResize={handleResize}
-                  />
-                ))}
-              </SortableContext>
-              <DragOverlay>
-                {activeItem ? (
-                  <ExerciseTimeCard item={activeItem} isOverlay={true} />
-                ) : null}
-              </DragOverlay>
-            </DndContext>
-          </div>
+          
+          {sData.map((data,idx) => (
+            <DisplayTrainingCard key={idx} data={data} cardHeight={cardHeight}/>
+          ))}
         </div>
       </div>
     </>
